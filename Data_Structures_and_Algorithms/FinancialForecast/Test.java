2d8d6a6414d8cf90c84cbe7b3f6e80f19496ae8d
Chinnu1805 <165566407+Chinnu1805@users.noreply.github.com>
2026-07-03 10:03:49 +0530

    Delete Data_Structures_and_Algorithms directory

--- Data_Structures_and_Algorithms/FinancialForecast/Test.java
+++
@@ -1,9 +0,0 @@
-
-public class Test {
-    public static void main(String[] args)
-    {
-    	Forecasting future=new Forecasting();
-    	double ans=future.FutureValue(15000.0,3);
-    	System.out.println("The Predicted Value is:"+ans);
-    }
-}
