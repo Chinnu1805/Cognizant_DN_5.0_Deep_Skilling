0ee9e7dc339516c4440fd8d36ea03ad430d21445
Chinnu1805 <165566407+Chinnu1805@users.noreply.github.com>
2026-07-05 23:06:18 +0530

    Add files via upload

---
+++ Week_3/Spring_REST_Using_SpringBoot3/Create_Authentication_Service_ThatReturns_JWT/AuthenticationController.java
@@ -0,0 +1,18 @@
+package com.cognizant.spring_learn.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.cognizant.spring_learn.model.AuthenticationResponse;
+
+@RestController
+public class AuthenticationController {
+
+    @GetMapping("/authenticate")
+    public AuthenticationResponse authenticate() {
+
+        String token = "Dummy-JWT-Token";
+
+        return new AuthenticationResponse(token);
+    }
+}
